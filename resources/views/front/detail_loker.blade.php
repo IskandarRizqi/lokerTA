34d597a334612b693091f5f3e10b9a7f98364a1d
dannykoes <sandal2610@gmail.com>
2024-06-04 23:55:11 +0700

    $v

--- resources/views/front/detail_loker.blade.php
+++ resources/views/front/detail_loker.blade.php
@@ -150,7 +150,7 @@
                                                 d="m16.267 10.563-.533 1.928C18.325 13.207 20 14.584 20 16c0 1.892-3.285 4-8 4s-8-2.108-8-4c0-1.416 1.675-2.793 4.267-3.51l-.533-1.928C4.197 11.54 2 13.623 2 16c0 3.364 4.393 6 10 6s10-2.636 10-6c0-2.377-2.197-4.46-5.733-5.437z">
                                             </path>
                                         </svg>
-                                        {{$loker->tempatperusahaan}}
+                                        {{$v->tempatperusahaan}}
                                     </div>
                                     {{-- <div class="alamat ml-2" style="font-size: 12px">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"
