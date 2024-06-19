0f9d63fcbd54abe5dee3263eeb909196b5499a52
dannykoes <sandal2610@gmail.com>
2024-06-19 23:10:53 +0700

    prfl

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function profile()
     {
-        return view('front.profile.index');
+        $x['data'] = User::where('id', Auth::user()->id)->first();
+        return view('front.profile.index', $x);
     }
 }
