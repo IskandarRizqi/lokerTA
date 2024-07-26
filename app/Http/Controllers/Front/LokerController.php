7fc323e55f35f17868bedb3c6826939818db53e0
dannykoes <sandal2610@gmail.com>
2024-07-27 01:54:36 +0700

    validasi submit loker

--- app/Http/Controllers/Front/LokerController.php
+++ app/Http/Controllers/Front/LokerController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\FormalModel;
+use App\Models\InformasiPribadiModel;
 use App\Models\InputdataModel;
 use App\Models\LamaranModel;
+use App\Models\NonFormalModel;
+use App\Models\PengalamanModel;
+use App\Models\SkillModel;
+use App\Models\SosialLinkModel;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -33,9 +39,29 @@ class LokerController extends Controller
             return Redirect::to('/')->with('info', 'Silahkan login terlebih dahulu');
         }
 
-        $auth = User::where('id', $id)->with('kriteria')->first();
-        if (!$auth->kriteria) {
-            return Redirect::back()->with('info', 'Silahkan isi data diri terlebih dahulu');
+        $infpbd = InformasiPribadiModel::where('id_user', Auth::id())->first();
+        if (!$infpbd) {
+            return Redirect::to('/informasipribadi')->with('info', 'Silahkan isi data diri terlebih dahulu');
+        }
+        $skill = SkillModel::where('id_user', Auth::id())->first();
+        if (!$skill) {
+            return Redirect::to('/skill')->with('info', 'Silahkan isi skill terlebih dahulu');
+        }
+        $pnglmn = PengalamanModel::where('id_user', Auth::id())->first();
+        if (!$pnglmn) {
+            return Redirect::to('/pengalaman')->with('info', 'Silahkan isi pengalaman terlebih dahulu');
+        }
+        $pdk = FormalModel::where('id_user', Auth::id())->first();
+        if (!$pdk) {
+            return Redirect::to('/formal')->with('info', 'Silahkan isi pendidikan formal terlebih dahulu');
+        }
+        $non = NonFormalModel::where('id_user', Auth::id())->first();
+        if (!$non) {
+            return Redirect::to('/nonformal')->with('info', 'Silahkan isi pendidikan non formal terlebih dahulu');
+        }
+        $sosial = SosialLinkModel::where('id_user', Auth::id())->first();
+        if (!$sosial) {
+            return Redirect::to('/sosiallink')->with('info', 'Silahkan isi sosial link terlebih dahulu');
         }
 
         $check = LamaranModel::where('loker_id', $id)->where('user_id', Auth::id())->first();
